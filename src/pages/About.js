90cce94b8cc1665d4394e9e1c826e4a317199db0
Vikash Kumar <vikkuvikash79097@gmail.com>
2023-03-26 00:02:35 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        A cocktail is an alcoholic mixed drink. Most commonly, cocktails are
+        either a single spirit or a combination of spirits, mixed with other
+        ingredients such as juices, flavored syrups, tonic water, shrubs, and
+        bitters. Cocktails vary widely across regions of the world, and many
+        websites publish both original recipes and their own interpretations of
+        older and more famous cocktails.
+      </p>
+    </section>
+  );
+};
+
+export default About;
